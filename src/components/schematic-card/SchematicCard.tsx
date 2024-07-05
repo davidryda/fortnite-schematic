a7dcd79c11c66fa9e7984be79c478fe0793ea24b
david <rydadave@gmail.com>
2024-07-05 15:58:52 -0400

    Rework Triangle Animations

--- src/components/schematic-card/SchematicCard.tsx
+++ src/components/schematic-card/SchematicCard.tsx
@@ -6,6 +6,14 @@ const SchematicCard = () => {
       <div className={styles.cardFrame}>
         <div className={styles.cardContent}>
           <div className={styles.cardContentSchematicAttributes}>
+            <div className={styles.schematicPowerLevel}>
+
+            </div>
+
+            <div className={styles.schematicClass}>
+
+            </div>
+
             <div className={styles.sunburst}>
               <div className={styles.wedge}></div>
               <div className={styles.wedge}></div>
@@ -21,11 +29,14 @@ const SchematicCard = () => {
               <div className={styles.wedge}></div>
 
               <div className={styles.triangle1}></div>
+              {/* <div className={styles.triangle1trailing}></div> */}
               <div className={styles.triangle2}></div>
               <div className={styles.triangle3}></div>
               <div className={styles.triangle4}></div>
               <div className={styles.triangle5}></div>
               <div className={styles.triangle6}></div>
+              <div className={styles.triangle10}></div>
+              <div className={styles.triangle10Trailing}></div>
             </div>
           </div>
           <div className={styles.cardContentLevels}></div>
