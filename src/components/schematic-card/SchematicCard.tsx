653ac1d865572964dcbf8531a89508a5d6934e8a
david <rydadave@gmail.com>
2024-08-10 18:06:42 -0400

    Add schematic details

--- src/components/schematic-card/SchematicCard.tsx
+++ src/components/schematic-card/SchematicCard.tsx
@@ -1,14 +1,18 @@
 import styles from './SchematicCard.module.scss';
+import antiCuddleSarahPortrait from "../../assets/heroes/portraits/Anti-Cuddle Sarah.webp"
 
 const SchematicCard = () => {
   return (
     <>
       <div className={styles.cardFrame}>
         <div className={styles.cardContent}>
+          <div className={styles.schematicPowerLevelContainer}></div>
           <div className={styles.cardContentSchematicAttributes}>
             <div className={styles.schematicPowerLevel}>
 
             </div>
+
+            <img className={styles.assetImage} src={antiCuddleSarahPortrait} />
 
             <div className={styles.schematicClass}>
 
@@ -39,7 +43,16 @@ const SchematicCard = () => {
               <div className={styles.triangle10Trailing}></div>
             </div>
           </div>
-          <div className={styles.cardContentLevels}></div>
+          <div className={styles.cardContentLevels}>
+            <div className={styles.levelBar}></div>
+            <div className={styles.starContainer}>
+              <div className={styles.star}></div>
+              <div className={styles.star}></div>
+              <div className={styles.star}></div>
+              <div className={styles.star}></div>
+              <div className={styles.star}></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
